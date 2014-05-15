e94cf6caaff27bc39cf041536e83e53e77b6c9bf
Skeltoras <git@skeltoras.de>
2014-05-15 22:19:35 +0200

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,30 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
+
   describe "Home page" do
+
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
-  
+
   describe "Help page" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 end
